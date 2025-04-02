e9bec669ec560fbee6ff804d4cb4434f6b287004
Renato SICLER <renatosicler33@gmail.com>
2025-04-02 13:37:25 +0200

    Ajoutez des fichiers projet.

---
+++ GildedRoseKata/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GildedRoseKata
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+    }
+}
---
+++ GildedRoseKata_Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace GildedRoseKata_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
